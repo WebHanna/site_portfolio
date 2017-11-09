9eef5d206fb4fde94fd7d772747e926ff38921fe
WebHanna <a.krupchak97@gmail.com>
2017-11-10 00:31:55 +0300

    start commit

--- www/js/custom.js
+++ www/js/custom.js
@@ -1,12 +1,37 @@
-$('.menu940').click(function () {
-    $('.menu_copy').css({
-        'display':'block'
-    });
+
+var a = false;
+
+$('.menu_icon').click(function () {
+
+    if(!a){
+        $('.menu_width_767px').css({
+            'display':'block'
+        });
+
+        a = !a;
+    } else {
+        $('.menu_width_767px').css({
+            'display':'none'
+        });
+
+        a = !a;
+    }
 });
 
-$('.close').click(function (e) {
-    e.stopPropagation();
-    $('.menu_copy').css({
-        'display':'none'
-    });
-});
+// $('.menu_icon').click(function (e) {
+//     e.stopPropagation();
+//     $('.menu_width_767px').css({
+//         'display':'none'
+//     });
+// });
+//
+// function showMenu('menu_width_767px'){
+//     var display = document.getElementById('menu_width_767px').style.display;
+//
+//     if(display === 'none'){
+//         document.getElementById('menu_width_767px').style.display='block';
+//
+//     }else{
+//         document.getElementById('menu_width_767px').style.display='none';
+//     }
+// }
